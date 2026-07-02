dfe15a3ebdd1e69951a5b393c8191d9fb18d7eb0
Tania Tatis <ttatisbatista@gmail.com>
2026-07-02 14:18:03 -0700

    add core and error handling blueprints, implement basic routes and error pages

--- socialblog/__init__.py
+++ socialblog/__init__.py
@@ -2,3 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from socialblog.core.views import core
+from socialblog.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
---
+++ socialblog/core/views.py
@@ -0,0 +1,15 @@
+# CORE/ VIEW.PY
+
+from flask import redirect,render_template, request, Blueprint
+
+core = Blueprint('core', __name__)
+
+@core.route('/')
+def index():
+    
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+
+    return render_template('info.html')
