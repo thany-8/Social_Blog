4ce77b7ca667c057cb5d561d1b6be5790b587121
Tania Tatis <ttatisbatista@gmail.com>
2026-07-02 18:59:49 -0700

    Fix User model: __tablename__, un-nest methods, use self.username

--- socialblog/models.py
+++ socialblog/models.py
@@ -9,8 +9,8 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
-    
-    __tablename___= 'users'
+
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     profile_image = db.Column(db.String(20),nullable=False,default='default_profile.png')
@@ -27,12 +27,12 @@ class User(db.Model, UserMixin):
         self.username = username
         self.password_hash = generate_password_hash(password)
 
-        def check_password(self,password):
-            return check_password_hash(self.password_hash, password)
-        
-        def __repr__(self):
-            return f"Username {username}"
-        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"Username {self.username}"
+
 
 
 
