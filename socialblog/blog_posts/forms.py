b806de1676f8bdde10ed072604771d08dc778c72
Tania Tatis <ttatisbatista@gmail.com>
2026-07-02 21:33:45 -0700

    Add CommentForm

--- socialblog/blog_posts/forms.py
+++ socialblog/blog_posts/forms.py
@@ -9,3 +9,8 @@ class BlogPostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     text = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('BlogPost')
+
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Post Comment')
